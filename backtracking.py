bd49e4b6f40f53d3252c11e64e36a70833df768c
infinity1013 <68748614+infinity1013@users.noreply.github.com>
2020-11-24 21:53:28 +0530

    Update backtracking.py

--- backtracking.py
+++ backtracking.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 
 app=Flask(__name__)
 
-@app.route('/',methods=["GET"])
+@app.route('/',methods=["GET","POST"])
 def random_board():
 	import Generate_random_board
 	solution_grid=Generate_random_board.solution
@@ -12,4 +12,6 @@ def random_board():
 @app.route('/backtracking',methods=["POST"])
 def backtracking():
 	return redirect(url_for("random_board"));
-app.run(debug=True)
+
+if __name__=="__main__":
+	app.run()
